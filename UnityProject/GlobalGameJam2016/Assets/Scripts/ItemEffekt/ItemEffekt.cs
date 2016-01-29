73fcc5d44624b38f8045fcdb3bf899af1ed575a3
unknown <Xells>
2016-01-29 23:22:45 +0100

    Added script and prefab "ItemEffekt" / Daniel

---
+++ UnityProject/GlobalGameJam2016/Assets/Scripts/ItemEffekt/ItemEffekt.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemEffekt : MonoBehaviour {
+
+    public float RotateSpeed = 1;
+    void FixedUpdate()
+    {
+        transform.Rotate(0, RotateSpeed * Time.deltaTime, 0);
+    }
+	
+}
