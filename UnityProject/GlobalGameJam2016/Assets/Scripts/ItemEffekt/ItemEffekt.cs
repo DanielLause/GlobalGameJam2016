a3bdf716f8b15742b59757be7475c7d1ee30cc46
Xells <Xellsnr1@hotmail.de>
2016-01-30 12:36:33 +0100

    Projektzusammenwurf

--- UnityProject/GlobalGameJam2016/Assets/Scripts/ItemEffekt/ItemEffekt.cs
+++ UnityProject/GlobalGameJam2016/Assets/Scripts/ItemEffekt/ItemEffekt.cs
@@ -3,10 +3,9 @@ using System.Collections;
 
 public class ItemEffekt : MonoBehaviour {
 
-    public float RotateSpeed = 1;
+    public float RotateSpeed = 45;
     void FixedUpdate()
     {
         transform.Rotate(0, RotateSpeed * Time.deltaTime, 0);
     }
-	
 }
