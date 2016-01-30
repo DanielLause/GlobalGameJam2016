2b36743cef166f9c252f3ef3cd396896fa8938e4
DanielLause <info@daniel-lause.de>
2016-01-30 18:16:34 +0100

    added item spawner

---
+++ UnityProject/GlobalGameJam2016/Assets/Scripts/ItemSpawn/ItemSpawn.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemSpawn : MonoBehaviour
+{
+
+    public List<GameObject> Items;
+    public List<Transform> SpawnPoints;
+
+    private List<Transform> constSpawnPoints;
+    private List<Transform> alreadyInUse;
+    int chosedItem;
+    int chosedSpawn;
+
+    void Awake()
+    {
+        constSpawnPoints = SpawnPoints;
+        alreadyInUse = new List<Transform>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("l"))
+        {
+            randomSpawn();
+        }
+    }
+
+    private void resetList()
+    {
+        SpawnPoints = constSpawnPoints;
+        alreadyInUse.Clear();
+    }
+    private void randomSpawn()
+    {
+        if (SpawnPoints.Count == 0)
+            return;
+
+        chosedSpawn = Random.Range(0, SpawnPoints.Count);
+        chosedItem = Random.Range(0, Items.Count);
+
+        Instantiate(Items[chosedItem], SpawnPoints[chosedSpawn].position, Quaternion.identity);
+        alreadyInUse.Add(SpawnPoints[chosedSpawn]);
+        SpawnPoints.Remove(SpawnPoints[chosedSpawn]);
+    }
+}
